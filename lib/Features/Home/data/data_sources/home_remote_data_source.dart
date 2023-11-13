07379492d2267a23b8da7609a3bcd064232874a7
FadyShehataa <fadyshehata40@gmail.com>
2023-11-13 17:05:15 +0200

    create home remote data source

---
+++ lib/Features/Home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:rick_and_morty_application/Features/Home/Domain/Entities/character_entity.dart';
+
+import '../../../../Core/utils/api_service.dart';
+import '../models/character_model/character_model.dart';
+
+abstract class HomeLocalDataSource {
+  Future<List<CharacterEntity>> fetchCharacters();
+}
+
+class HomeLocalDataSourceImpl implements HomeLocalDataSource {
+  ApiService apiService;
+  HomeLocalDataSourceImpl({required this.apiService});
+
+  @override
+  Future<List<CharacterEntity>> fetchCharacters() async {
+    var data = await apiService.get(endPoint: 'character?page=1');
+    List<CharacterEntity> characters = parseCharacters(data);
+    return characters;
+  }
+
+  List<CharacterEntity> parseCharacters(Map<String, dynamic> data) {
+    List<CharacterEntity> characters = [];
+    for (var item in data['results']) {
+      characters.add(CharacterModel.fromJson(item));
+    }
+    return characters;
+  }
+}
