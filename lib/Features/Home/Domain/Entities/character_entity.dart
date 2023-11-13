36da782c82e58ad76e7ab50c8cf1455f87c2d2cd
FadyShehataa <fadyshehata40@gmail.com>
2023-11-13 16:52:37 +0200

    create character model

--- lib/Features/Home/Domain/Entities/character_entity.dart
+++ lib/Features/Home/Domain/Entities/character_entity.dart
@@ -1,17 +1,17 @@
 class CharacterEntity {
-  final String? image;
-  final String? name;
-  final String? status;
-  final String? species;
-  final String? type;
-  final String? gender;
+  final String? characterImage;
+  final String? characterName;
+  final String? characterStatus;
+  final String? characterSpecies;
+  final String? characterType;
+  final String? characterGender;
 
   CharacterEntity({
-    required this.image,
-    required this.name,
-    required this.status,
-    required this.species,
-    required this.type,
-    required this.gender,
+    required this.characterImage,
+    required this.characterName,
+    required this.characterStatus,
+    required this.characterSpecies,
+    required this.characterType,
+    required this.characterGender,
   });
 }
