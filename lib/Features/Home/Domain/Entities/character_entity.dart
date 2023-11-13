f882df09900753266b5d88cc1cd116815dba4cd5
FadyShehataa <fadyshehata40@gmail.com>
2023-11-13 15:28:15 +0200

    create character entity

---
+++ lib/Features/Home/Domain/Entities/character_entity.dart
@@ -0,0 +1,17 @@
+class CharacterEntity {
+  final String? image;
+  final String? name;
+  final String? status;
+  final String? species;
+  final String? type;
+  final String? gender;
+
+  CharacterEntity({
+    required this.image,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+  });
+}
